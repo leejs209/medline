18d9fdc8e8b05fb39619c53a773b327862a1f5fb
Jun-Seong Lee <leejs209209@gmail.com>
2020-08-15 03:45:17 +0900

    Fixed some stuff

---
+++ medline/runner.sh
@@ -0,0 +1,2 @@
+./manage.py collectstatic --noinput
+uwsgi --http "0.0.0.0:${PORT}" --module api.wsgi --master --processes 4 --threads 2
--- medline/settings.py
+++ medline/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_fqy2e7@ad=vgh)wy3@jy4gnv6psd)knyv1$53$&rpy@efbr85'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = False
 DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# STATIC_ROOT = "/Users/leejs/static"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
