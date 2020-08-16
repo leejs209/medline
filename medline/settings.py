45f9512167296277f23da06412ccb2801089101e
Jay Lee <leejs2090209@gmail.com>
2020-08-17 03:50:09 +0900

    Heroku

--- medline/settings.py
+++ medline/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,3 +146,6 @@ PWA_APP_ORIENTATION = 'any'
 PWA_APP_START_URL = '/'
 PWA_APP_DIR = 'ltr'
 PWA_APP_LANG = 'ko-KR'
+
+
+django_heroku.settings(locals())
