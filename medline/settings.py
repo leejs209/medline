6ee23183513f62a8629e92f1940fc64f29562e63
Jay Lee <leejs2090209@gmail.com>
2020-08-17 03:55:20 +0900

    Heroku

--- medline/settings.py
+++ medline/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "docs", "static")
-STATIC_URL = 'https://leejs209.github.io/medline/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
