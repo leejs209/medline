243371adf7902f9bbaba791cd72f53c01473baa4
Jun-Seong Lee <leejs209209@gmail.com>
2020-08-17 14:00:54 +0900

    Update settings.py

--- medline/settings.py
+++ medline/settings.py
@@ -13,6 +13,18 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import django_heroku
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://a0268478008646fb9985137c7f7234b0@o434980.ingest.sentry.io/5392651",
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
